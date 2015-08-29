1c71a8c81a06616c444f7fd4197aa2a5c938135f
Daniel Perez Alvarez <unindented@gmail.com>
2015-08-29 20:38:15 +0200

    Add tray icon

--- src/main/index.js
+++ src/main/index.js
@@ -3,20 +3,47 @@
 const path = require('path')
 const electron = require('electron')
 const app = electron.app
+const Tray = electron.Tray
 const BrowserWindow = electron.BrowserWindow
 
+let tray = null
 let win = null
+
+const createTray = () => {
+  const iconPath = path.resolve(__dirname, '../../resources/IconTemplate.png')
+
+  tray = new Tray(iconPath)
+
+  tray.on('click', () => {
+    if (win.isVisible()) {
+      win.hide()
+    } else {
+      win.show()
+    }
+  })
+
+  tray.on('double-click', () => {
+    win.destroy()
+  })
+}
 
 const createWindow = () => {
   const winUrl = 'file://' + path.resolve(__dirname, '../renderer/index.html')
 
   win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    show: false
   })
   win.loadURL(winUrl)
 
+  win.on('close', (evt) => {
+    evt.preventDefault()
+    win.hide()
+  })
+
   win.on('closed', () => {
+    tray = null
     win = null
   })
 }
@@ -26,5 +53,6 @@ app.on('window-all-closed', () => {
 })
 
 app.on('ready', () => {
+  createTray()
   createWindow()
 })
